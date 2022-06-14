a275e2de17e3eecfab566da7a5967151d3b051a0
Shaked Erinberg <shakede@wix.com>
2022-06-14 10:39:43 +0300

    names

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import InstagramPost from './components/InstagramPost';
 import { st, classes } from './style/main.st.css';
-import { POST_IMAGE, PROFILE_PICTURE, POST_DESCRIPTION, USER_NAME } from './constants';
+import { SAMPLE_POST_IMAGE, PROFILE_PICTURE, POST_DESCRIPTION, USER_NAME } from './constants';
 
 export const App = () => (
     <main className={st(classes.root)}>
         <InstagramPost
-            img={POST_IMAGE}
+            img={SAMPLE_POST_IMAGE}
             userName={USER_NAME}
             description={POST_DESCRIPTION}
             profilePicture={PROFILE_PICTURE}
--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,6 @@ export const FIRST_LIKED_BY_PROFILE_PICTURE =
     'https://cdn.pixabay.com/photo/2021/06/04/10/28/portrait-6309448_960_720.jpg';
 export const SECOND_LIKED_BY_PROFILE_PICTURE =
     'https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/80/posts/26761/image/27-MalePortraits.jpg';
-export const POST_IMAGE =
+export const SAMPLE_POST_IMAGE =
     'https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80';
 export const PLACE_HOLDER = 'Add a comment...';
