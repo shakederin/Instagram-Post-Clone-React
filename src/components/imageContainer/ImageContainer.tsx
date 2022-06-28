a0f82cc43683b3d47d66176dcabb6f3356367757
Shaked Erinberg <shakede@wix.com>
2022-06-28 12:22:51 +0300

    fix profilePicture

--- src/components/imageContainer/ImageContainer.tsx
+++ src/components/imageContainer/ImageContainer.tsx
@@ -8,8 +8,8 @@ interface IImageContainerProps {
 const ImageContainer = ({ imgUrls }: IImageContainerProps) => (
     <div className={classes.root}>
         <div className={classes.mainImg} style={{ backgroundImage: `url(${imgUrls[0]})` }}>
-            <ScrollButton className={classes.arrow} svgPath={arrow} />
-            <ScrollButton className={st(classes.rightArrow, classes.arrow)} svgPath={arrow} />
+            <ScrollButton className={st(classes.arrow, { direction: 'left' })} svgPath={arrow} />
+            <ScrollButton className={st(classes.arrow, { direction: 'right' })} svgPath={arrow} />
         </div>
     </div>
 );
