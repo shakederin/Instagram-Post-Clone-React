3b2092f69d1bf2d04826459caae4a0c4f0823c04
Shaked Erinberg <shakede@wix.com>
2022-06-29 16:43:30 +0300

    cashed

--- src/test/tests/addComment.test.ts
+++ src/test/tests/addComment.test.ts
@@ -1,5 +1,28 @@
+import { AddCommentDriver } from '../testKit/addComment/addCommentDriver';
+import { Browser, chromium, Page } from 'playwright';
+const port = 3000;
+
 describe('add commment ', () => {
-    it('add comment with Enter', () => {});
+    let browser: Browser;
+    let page: Page;
+    before(async () => {
+        browser = await chromium.launch();
+    });
+    after(async () => {
+        await browser.close();
+    });
+    beforeEach(async () => {
+        page = await browser.newPage();
+    });
+    afterEach(async () => {
+        await page.close();
+    });
+    beforeEach(async () => {
+        await page.goto(`http://localhost:${port}/`);
+    });
+    it('add comment with Enter', () => {
+        const instagramDriver = new AddCommentDriver(page);
+    });
     it('add comment with Post button', () => {});
     it('add comment with invalide value', () => {});
 });
