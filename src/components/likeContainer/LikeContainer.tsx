3dd8268b8b6afde57096b051137663f2646704cd
Shaked Erinberg <shakede@wix.com>
2022-06-30 18:17:13 +0300

    delete comments - /* */

--- src/components/likeContainer/LikeContainer.tsx
+++ src/components/likeContainer/LikeContainer.tsx
@@ -5,8 +5,6 @@ import { classes } from './LikeContainer.st.css';
 import type { IinisialState, User } from '../../types';
 import { getRandomUser } from '../../utils/getRandomUser';
 
-// type userMate = Record<string, User>;
-
 const LikeContainer = () => {
     const randomUser = getRandomUser();
     const [usersWhoLiked, setUsersWhoLiked] = useState<IinisialState>({
@@ -31,28 +29,3 @@ const LikeContainer = () => {
 };
 
 export default LikeContainer;
-
-// const likePost = (user: User) => {
-//     setUsersWhoLiked((prev) => {
-//         const userIndex = prev.findIndex(
-//             (existingUser) => existingUser.userName === user.userName
-//         );
-//         return userIndex === -1
-//             ? [...prev, user]
-//             : [...prev.slice(0, userIndex), ...prev.slice(userIndex + 1)];
-//     });
-// };
-
-// const likePost = (user: User) => {
-//     let currentUsersWhoLiked = [...usersWhoLiked];
-//     const filteredArray = currentUsersWhoLiked.filter(
-//         (user) => user.userName === user.userName
-//     )[0];
-//     const indexOfUser = currentUsersWhoLiked.indexOf(filteredArray);
-//     if (indexOfUser < 0) {
-//         setUsersWhoLiked([...currentUsersWhoLiked, user]);
-//     } else {
-//         currentUsersWhoLiked.splice(indexOfUser, 1);
-//         setUsersWhoLiked(currentUsersWhoLiked);
-//     }
-// };
